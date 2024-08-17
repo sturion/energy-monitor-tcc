c73e7c3b88f75cc34acbc29b16ad2b46d90aed07
Douglas Sturion <sturion.douglas@gmail.com>
2024-08-17 10:49:39 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:energy_monitor/chart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Energy monitor'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 10,),
+          Row(
+            children: [
+              SizedBox(width: 10,),
+              ElevatedButton(
+                onPressed: _incrementCounter,
+                child: Row(
+                  children: [
+                    Icon(Icons.bolt, size: 46),
+                    Column(
+                children: [
+                  Text("Computador",style: TextStyle(fontSize: 20,color: Colors.black),),
+                  Text("Desligado",style: TextStyle(fontSize: 16,color: Colors.grey),),
+                ],
+              )
+                  ],
+                ),
+              ),
+              
+            ],
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+          LineChartSample1(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
